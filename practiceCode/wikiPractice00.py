efb23ad385d077056c0090e58ef2da69b388ce3b
Dongkeun.YOON <31213371+Bronzeroot@users.noreply.github.com>
2020-11-20 12:54:22 +0900

    Create wikiPractice00.py

---
+++ practiceCode/wikiPractice00.py
@@ -0,0 +1,7 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+html = urlopen('http://en.wikipedia.org/wiki/Kevin_Bacon')
+bs = BeautifulSoup(html, 'html.parser')
+for link in bs.findAll('a'):
+    if 'href' in link.attrs:
+        print(link.attrs['href'])
