509e01694e53bacf1ca01f89c0b723225567b6d4
Dongkeun.YOON <31213371+Bronzeroot@users.noreply.github.com>
2020-11-23 08:26:48 +0900

    Create wikiPractice01.py

---
+++ practiceCode/wikiPractice01.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import datetime
+import random
+import re
+
+random.seed(datetime.datetime.now())
+
+def getLinks(articleUrl):
+    html = urlopen('http://en.wikipedia.org{}'.format(articleUrl))
+    bs = BeautifulSoup(html,'html.parser')
+    return bs.find('div',{'id':'bodyContent'}).findll('a',href = re.compile('^(/wiki/)((?!:).)*$'))
+
+links = getLinks('/wiki/Kevin_Bacon')
+while len(links)>0 :
+    newArticle = links[random.randint(0,len(links)-1)].attrs['href']
+    print(newArticle)
+    links = getLinks(newArticle)
